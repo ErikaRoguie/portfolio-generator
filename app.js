a2eae9c168b48f50de1684a38ac08978a8789dd9
Erika <88861951+ErikaRoguie@users.noreply.github.com>
2022-02-12 16:53:50 -0700

    intell

--- app.js
+++ app.js
@@ -16,7 +16,15 @@ const promptUser =() => {
         {
             type: 'input',
             name: 'name',
-            message: 'What is your name?'
+            message: 'What is your name? (Required)',
+            validate: nameInput =>{
+                if(nameInput){
+                    return true;
+                } else{
+                    console.log('Please enter your name!');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
@@ -24,9 +32,21 @@ const promptUser =() => {
             message: 'Enter your GitHub Username'
         },
         {
+            type: 'confirm',
+            name: 'ConfirmAbout',
+            message: 'Would you like to enter some information about yourself for an "About" section?',
+            default: true
+        },
+        {
             type: 'input',
             name: 'about',
-            message: 'Provide some information about yourself:'
+            message: 'Provide some information about yourself:',when: ({ confirmAbout }) => {
+                if (confirmAbout) {
+                return true;
+                } else {
+                return false;
+                }
+            }
         }
     ]);
 };
