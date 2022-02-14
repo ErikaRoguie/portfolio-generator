37afdbbfff8a385a8d9fea0cedfa30fbd1382be0
Erika <88861951+ErikaRoguie@users.noreply.github.com>
2022-02-14 14:17:48 -0700

    header, about, and projects

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
-const generatePage = require('./9.3/src/page-template');
+const generatePage = require('./src/page-template');
 
 const promptUser = () => {
   return inquirer.prompt([
@@ -128,12 +128,12 @@ Add a New Project
 
 promptUser()
   .then(promptProject)
-  .then(portfolioData => {
-    console.log(portfolioData);
+  .then(portfolioData => {    
+    const pageHTML = generatePage();
     // will be uncommented in lesson 4
-    // const pageHTML = generatePage(portfolioData);
-    // fs.writeFile('./index.html', pageHTML, err => {
-    //   if (err) throw new Error(err);
-    //   console.log('Page created! Check out index.html in this directory to see it!');
-    // });
+     const pageHTML = generatePage(portfolioData);
+     fs.writeFile('./index.html', pageHTML, err => {
+       if (err) throw new Error(err);
+       console.log('Page created! Check out index.html in this directory to see it!');
+     });
   });
